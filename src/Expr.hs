dcd999ba0e4e3c49ed7dfe0a6f8a4dedbeb9c270
roboguy13 <roboguy06@gmail.com>
2021-10-30 02:28:01 -0500

    Function calls

--- src/Expr.hs
+++ src/Expr.hs
@@ -4,15 +4,25 @@ module Expr
 import           Name
 import           Ppr
 
+import           Data.List
+
 data Expr a
   = Var Name
   | Lit Int
   | Sub (Expr a) (Expr a)
+  | Call FnCallName [Expr a]
   | ModuleMember a Name  -- | @ModuleName "M" "x"@  ===  @M.x@
+
+data FnCallName = FnCallLocal Name | FnCallModule ModuleName Name
+
+instance Ppr FnCallName where
+  ppr (FnCallLocal n) = ppr n
+  ppr (FnCallModule mod n) = ppr mod ++ "." ++ ppr n
 
 instance Ppr a => Ppr (Expr a) where
   ppr (Var v) = ppr v
   ppr (Lit i) = show i
   ppr (Sub x y) = '(' : ppr x ++ ") - (" ++ ppr y ++ ")"
+  ppr (Call n args) = ppr n ++ "(" ++ intercalate ", " (map ppr args) ++ ")"
   ppr (ModuleMember mod n) = ppr mod ++ "." ++ ppr n
 
